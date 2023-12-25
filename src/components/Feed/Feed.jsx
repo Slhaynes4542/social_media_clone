0fe773d9bab2da81950c38e09abe7414249cddfb
Samuel Haynes <slhaynes4542@ksu.edu>
2023-12-25 16:00:53 -0600

    map data to posts in feed

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -2,15 +2,21 @@ import { useState } from 'react'
 import Post from '../Post/Post.jsx'
 import star_image from '../../assets/stars_background.jpg'
 import summer21 from '../../assets/summer.png'
+import jsonData from '../../data/data.json'
 
 function Feed() {
+  
 
   return (
     <>
     <div className="feed">
-      <Post name="Samuel Haynes" image={star_image}/>
-      <Post name="Samuel Haynes" image={summer21}/>
-      <Post name="Daniel Kins"/>
+      
+      {
+        jsonData.posts.map( (item)=>(
+          <Post name={item.name} image={item.img_url} text={item.message}></Post>
+        ))
+      }
+
     </div>
 
   
